872f27441a5ae7dcc4dbe97071fdc899086e64ca
Tyler Williams <tyler.williams5471235@gmail.com>
2020-09-10 11:23:39 -0500

    comments

--- sierpinski_gasket.py
+++ sierpinski_gasket.py
@@ -5,8 +5,9 @@ point_lst = []
 
 for y in range(0,256):
     for x in range(0, 256):
-        if (x & (y-x)) == 0:
-            point_lst.append([x+128-.5*y, 256-y]) # replacing 256-y with y will flip the image
+        if (x & (y-x)) == 0: 
+            point_lst.append([x+128-.5*y, 256-y]) # replacing y with 256-y will flip the image, since with y it is 
+                                                  # upside-down
 
 for p in point_lst:
     plt.scatter(p[0], p[1], s=1, c='black', marker='s')
